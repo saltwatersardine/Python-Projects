ff8705ae01a7aff3ad40c3f2643c24f0748e6977
saltwatersardine <matthewhugo5@gmail.com>
2022-08-04 09:54:58 +0200

    Update card_roulette.py

--- card_roulette.py
+++ card_roulette.py
@@ -1,12 +1,10 @@
 import random
 
-random.seed()
+names_string = input("Give me everybody's names, seperated by a comma. ")
+names = names_string.split(", ")
 
-names_in_string = input("Give me everybody's names, separated by a comma. ")
-names_list = names_in_string.split(", ")
-
-number_of_items = len (names_list)
-random_person = random.randint(0, number_of_items -1)
-who_pays = names_list[random_person]
-
-print(who_pays + " is going to buy the meal today!")
+num_items = len(names)
+random_choice = random.randint(0, num_items - 1)
+person_paying = names[random_choice]
+#person_paying = random.choice(names) - this will do the same as line 6-8
+print(person_paying + " is going to buy the meal today!")
